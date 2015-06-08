09c2c5cd2ffbd7ee6ea2c356825210068634a043
mteng211 <jykell@Michaels-MacBook-Pro.local>
2015-06-08 05:42:32 -0700

    working analyses

--- analyze.py
+++ analyze.py
@@ -2,9 +2,14 @@
 import json
 import sys
 from pprint import pprint
-import pydot
+ispydot = True
+try:
+  import pydot
+  ispydot = True
+except:
+  print "WARNING - you do not have pydot installed, this is needed for the callgraph"
+  ispydot = False
 import subprocess
-import time
 
 
 # recursively find all the direct calls given the original dict, which we know is a function
@@ -12,7 +17,7 @@ def find_calls(json_dict, func_calls):
   if "Type" in json_dict:
     if json_dict["Type"] == "Call":
       if json_dict["Call"]["Calltype"] == "direct": 
-        func_calls.append(json_dict["Call"]["Callee"])
+        func_calls.append(str(json_dict["Call"]["Callee"]))
      #   # extract arguments
      #   args_list = json_dict["Call"]["Args"]
      #   type_name_list = []
@@ -30,7 +35,24 @@ def find_calls(json_dict, func_calls):
       for entry in json_dict[key]:
         if type(entry) == dict:
           find_calls(entry, func_calls)
-  
+
+# recursively find function calls that do not get assigned to a variable 
+def find_unassigned(json_dict, calls):
+  for key in json_dict:
+    if type(json_dict[key]) == dict:
+      if "Call" in json_dict[key]:
+        if json_dict[key]["Call"]["Calltype"] == "direct":
+          if key == "Rhs":
+            continue
+          else:
+            calls.append(str(json_dict[key]["Call"]["Callee"]) + " is not assigned to a variable") 
+    if type(json_dict[key]) == dict:
+      find_unassigned(json_dict[key], calls)
+    elif type(json_dict[key]) == list:
+      for entry in json_dict[key]:
+        if type(entry) == dict:
+          find_unassigned(entry, calls)
+
 # recursive dfs to find transitive calls
 def cg_dfs(callees, x, visited, call_graph_as_dict):
   for node in callees:
@@ -43,6 +65,22 @@ def cg_dfs(callees, x, visited, call_graph_as_dict):
         cg_dfs(call_graph_as_dict[node], x, visited, call_graph_as_dict)
       except:
         continue
+
+#recursive dfs to find all modified variables in a function
+def ms_dfs(var_list, json_dict):
+  if "Var" in json_dict:
+    var_dict = json_dict["Var"]
+    if "Init" in var_dict:
+      if var_dict["Name"] not in var_list:
+        var_list.append(str(var_dict["Name"]))
+  for key in json_dict:
+    if type(json_dict[key]) == dict:
+      ms_dfs(var_list, json_dict[key])
+    elif type(json_dict[key]) == list:
+      for entry in json_dict[key]:
+        if type(entry) == dict:
+          ms_dfs(var_list, entry)
+      
  
 def main():
   # declare some random shit here
@@ -54,6 +92,8 @@ def main():
   json_structures = []
   calls = {}
   global_vars = {}
+  mod_set = {}
+  unassigned = {}
   with open(sys.argv[1], 'r') as code_data:
     # each line in the file is a seperate json data object
     for line in code_data.readlines():
@@ -65,31 +105,39 @@ def main():
       if "Type" in data:
         if data["Type"] == "Function":
           caller = data["Name"]
-          calls[caller] = []
+          calls[str(caller)] = []
+          mod_set[str(caller)] = []
           find_calls(data, calls[caller])
+          ms_dfs(mod_set[caller], data)
+          unassigned[str(caller)] = [] 
+          find_unassigned(data, unassigned[caller])
         elif data["Type"] == "Var":
           var_dict = data["Var"]
           name = var_dict["Name"]
           vartype = var_dict["Type"]
-          global_vars[vartype + " " + name] = "Unitialized"
+          global_vars[str(vartype) + " " + str(name)] = "Unitialized"
           if "Init" in var_dict:
-            global_vars[vartype + " " + name] = "Initialized"
+            global_vars[str(vartype) + " " + str(name)] = "Initialized"
                  
       if type(data) == dict:
         json_structures.append(data)
-  #pprint(calls)
+
   # create the call graph
-  graph = pydot.Dot(graph_type='digraph')
-  
-  for node_a in calls:
-    for node_b in calls[node_a]:
-      edge = pydot.Edge(node_a, node_b)
-      graph.add_edge(edge) 
-  
-  print "Current static analyses includes: " 
+  if ispydot:
+    graph = pydot.Dot(graph_type='digraph')
+    
+    for node_a in calls:
+      for node_b in calls[node_a]:
+        edge = pydot.Edge(node_a, node_b)
+        graph.add_edge(edge) 
+    
+  print "\nCurrent static analyses includes: " 
   print "   - reachability"
   print "   - show callgraph" 
   print "   - globals" 
+  print "   - mod set"
+  print "   - global functions"
+  print "   - unassigned returns"
   print "Type 'quit' to exit the analyzer"
 
   while 1:
@@ -108,22 +156,47 @@ def main():
         for key in calls:
           print "   - " + key
     elif str_input == 'show callgraph':
+      if ispydot: 
         graph.write_png('callgraph.png')
         try:
           subprocess.call('open callgraph.png', shell=True)
           subprocess.call('eog callgraph.png', shell=True)
         except:
-          print "Callgraph stored as temp_callgraph.png"
+          print "Callgraph stored as callgraph.png"
+      else:
+        print "you do not have pydot installed, please do not use this command"
     elif str_input == 'globals':
+        print "\n Global variables are:"
         pprint(global_vars) 
+    elif str_input == 'mod set':
+      print "\n The mod set for the functions are: "  
+      pprint(mod_set)
+    elif str_input == 'global functions':
+      print "\nThe follow functions are able to modify global variables:"
+      temp = []
+      temp2 = []
+      for key in global_vars:
+        x = key.split(" ")
+        temp.append(x[1])
+      for key in mod_set:
+        for node in mod_set[key]:
+          if node in temp:
+            temp2.append(key)
+      a = set(temp2)
+      pprint(a)
+    elif str_input == 'unassigned returns':
+      pprint(unassigned) 
     elif str_input == 'quit':
       sys.exit()
     else:
-      print "command not recognized, please try again\n"
+      print "\ncommand not recognized, please try again\n"
       print "Current static analyses includes: " 
       print "   - reachability"
       print "   - show callgraph" 
       print "   - globals" 
+      print "   - mod set"
+      print "   - global functions"
+      print "   - unassigned returns"
       print "Type 'quit' to exit the analyzer"
 
   return 0
